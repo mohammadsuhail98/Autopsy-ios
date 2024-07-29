01cf42816675791a9852fcbbb137681a2ecf0243
mohammadsuhail98 <msuhayl@earthlinktele.com>
2024-07-29 17:06:55 +0200

    First Commit.

---
+++ Autopsy/AutopsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutopsyApp.swift
+//  Autopsy
+//
+//  Created by mohammad suhail on 29/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AutopsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Autopsy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Autopsy
+//
+//  Created by mohammad suhail on 29/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
