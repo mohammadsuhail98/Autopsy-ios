f4d5b6b000fb0d7443c634a579e35afe1481f322
mohammadsuhail98 <msuhayl@earthlinktele.com>
2024-07-29 18:56:41 +0200

    Setup main screen.

--- Autopsy/ContentView.swift
+++ Autopsy/ContentView.swift
@@ -10,13 +10,53 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Spacer()
+            Image("main_screen_logo")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 100, trailing: 0))
+            Button(action: {
+                
+            }) {
+                Label("New Case", image: "new_case")
+                .frame(width: 250, height: 40)
+                .padding()
+                .background(Color.white)
+                .foregroundColor(Color.textColor)
+                .clipShape(.rect(cornerRadii: .init(topLeading: 5, bottomLeading: 5, bottomTrailing: 5, topTrailing: 5)))
+                .font(Font(CTFont(.label, size: 15)))
+                .bold()
+                .shadow(color: .shadow, radius: 2, x: 1, y: 1)
+                .dialogIcon(Image("new_case"))
+            }
+
+            Button(action: {
+                
+            }) {
+                Label("Open Case", image: "open_case")
+                .frame(width: 250, height: 40)
+                .padding()
+                .background(Color.white)
+                .foregroundColor(Color.textColor)
+                .clipShape(.rect(cornerRadii: .init(topLeading: 5, bottomLeading: 5, bottomTrailing: 5, topTrailing: 5)))
+                .font(Font(CTFont(.label, size: 15)))
+                .bold()
+                .shadow(color: .shadow, radius: 2, x: 1, y: 1)
+                .dialogIcon(Image("new_case"))
+            }
+            
+            Spacer()
+            
+            Text("Online Autopsy Documentation>")
+                .font(Font(CTFont(.label, size: 13)))
+                .fontWeight(.light)
+            
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.background)
     }
+    
 }
 
 #Preview {
