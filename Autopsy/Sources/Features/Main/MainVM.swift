c453ffc06f5a83cc14e3e9a4437e3644a26b2fdc
mohammadsuhail98 <msuhayl@earthlinktele.com>
2024-08-29 15:09:29 +0200

    Sort cases in main screen via date.

--- Autopsy/Sources/Features/Main/MainVM.swift
+++ Autopsy/Sources/Features/Main/MainVM.swift
@@ -17,11 +17,24 @@ class MainVM: ObservableObject {
     func getCases(){
         CasesManager.getCases { [weak self] cases in
             guard let self else { return }
-            self.recentCases = cases.reversed()
+            self.recentCases = sortedCases(cases: cases)
         } errorBlock: { [weak self] error in
             guard let self else { return }
             self.errMsg = error.errorMsg
             self.showErrorPopup = true
+        }
+    }
+    
+    func sortedCases(cases: [CaseEntity]) -> [CaseEntity] {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+
+        return cases.sorted { case1, case2 in
+            if let date1 = dateFormatter.date(from: case1.formattedDate),
+               let date2 = dateFormatter.date(from: case2.formattedDate) {
+                return date1 > date2
+            }
+            return false
         }
     }
     
